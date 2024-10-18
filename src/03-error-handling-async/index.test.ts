fae1a4ef8773576a468ccdd804e4c5ae0c926821
Anhelina Mahidava <dinalla88@gmail.com>
2024-10-18 21:49:39 +0300

    Added more tests

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,30 +1,52 @@
-// Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import {
+  resolveValue,
+  throwError,
+  throwCustomError,
+  MyAwesomeError,
+  rejectCustomError,
+} from './index';
 
 describe('resolveValue', () => {
-  test('should resolve provided value', async () => {
-    // Write your test here
+  describe('resolveValue', () => {
+    test.each([
+      [26, 26],
+      ['test', 'test'],
+      [{ key: 'test' }, { key: 'test' }],
+      [null, null],
+    ])('should resolve provided value %p', async (value, expected) => {
+      const result = await resolveValue(value);
+      expect(result).toEqual(expected);
+    });
   });
 });
 
 describe('throwError', () => {
-  test('should throw error with provided message', () => {
-    // Write your test here
-  });
-
-  test('should throw error with default message if message is not provided', () => {
-    // Write your test here
+  test.each([
+    ['Custom error message', 'Custom error message'],
+    [undefined, 'Oops!'],
+  ])('should throw error with message %p', (input, expectedMessage) => {
+    expect(() => throwError(input)).toThrowError(expectedMessage);
   });
 });
 
 describe('throwCustomError', () => {
-  test('should throw custom error', () => {
-    // Write your test here
-  });
+  test.each([[MyAwesomeError, 'This is my awesome custom error!']])(
+    'should throw custom error %p with message %s',
+    (errorClass, expectedMessage) => {
+      expect(() => throwCustomError()).toThrowError(errorClass);
+      expect(() => throwCustomError()).toThrowError(expectedMessage);
+    },
+  );
 });
 
 describe('rejectCustomError', () => {
-  test('should reject custom error', async () => {
-    // Write your test here
-  });
+  test.each([
+    [MyAwesomeError, 'This is my awesome custom error!'], // Rejected custom error with message
+  ])(
+    'should reject custom error %p with message %s',
+    async (errorClass, expectedMessage) => {
+      await expect(rejectCustomError()).rejects.toThrowError(errorClass);
+      await expect(rejectCustomError()).rejects.toThrowError(expectedMessage);
+    },
+  );
 });
